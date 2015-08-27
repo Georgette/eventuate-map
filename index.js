9b04ff1de852b96e7ad39d2078a67d5e82917311
Georgette Pincin <georgette@pincin.com>
2015-08-26 22:42:50 -0400

    add tests

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 module.exports = function mkMappedEventuate (eventuate, map) {
+    // create a new eventuate with the parent's setting
     var mappedEventuate = eventuate.factory({ monitorConsumers: eventuate.hasConsumer !== undefined })
+
+    // expose to external sources
     mappedEventuate.upstreamConsumer = mapConsumer
+
+    // destroy upstream consumer
     mappedEventuate.unsubscribe = function mappedEventuateUnsubscribe () {
         eventuate.removeConsumer(mapConsumer)
     }
 
+    // create new eventuate upstream consumer
     eventuate(mapConsumer)
+
     return mappedEventuate
 
     function mapConsumer (data) {
+        // act on original producers payload, and emit(produce) event
         mappedEventuate.produce(map(data))
     }
 }
--- test/eventuate-map.js
+++ test/eventuate-map.js
@@ -7,8 +7,44 @@ test('should be a function', function (t) {
     t.end()
 })
 
-test('should map events', function (t) {
+test('eventuate map', function (t) {
+    t.plan(8)
+
     var event = eventuate()
-    
-    t.end()
+    var multiplyTen = map(event, function (v) { return v * 10 })
+
+    t.ok(~event.consumers.indexOf(multiplyTen.upstreamConsumer), 'adds consumer to upstream event')
+
+    t.ok(multiplyTen.consumerAdded, 'has consumerAdded')
+    t.ok(multiplyTen.consumerRemoved, 'has consumerRemoved')
+    t.ok(multiplyTen.hasConsumer !== undefined, 'has hasConsumer')
+
+    var eventCount = 0
+    event(function () {
+        eventCount++
+    })
+
+    var multiplyTenCount = 0
+    var mappedValues = []
+    multiplyTen(function (v) {
+        mappedValues.push(v)
+        multiplyTenCount++
+        if (multiplyTenCount === 2) {
+            t.deepEqual(mappedValues, [20, 10], 'should give multiplication of 10 in order')
+        }
+    })
+
+    t.true(multiplyTen.hasConsumer, 'registers consumers')
+
+    event.produce(2)
+    event.produce(1)
+
+    // after unsubscribe, no more events should propogate
+    multiplyTen.unsubscribe()
+    t.notOk(~event.consumers.indexOf(multiplyTen.upstreamConsumer), 'unsubscribe removes consumer from upstream event')
+    event.produce(1)
+    event.produce(1)
+
+    t.equal(eventCount, 4, 'produce 4 events')
+
 })
